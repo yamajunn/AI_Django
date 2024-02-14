1a350b3f5b194b01b464370a82cf29c093470950
yamajunn <amiton1115@gmail.com>
2024-02-15 01:58:52 +0900

    daaaaaaaa

--- skin/home/urls.py
+++ skin/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+    path('', views.home, name='home'),
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)

fa5a3e7dc1628679c736961bd9880cdd20e7daa3
yamajunn <amiton1115@gmail.com>
2024-02-15 17:03:53 +0900

    aaaaa

--- skin/home/urls.py
+++ skin/home/urls.py
@@ -5,4 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('list', views.list, name='list'),
+    path('make', views.make, name='make'),
+    path('use', views.use, name='use'),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- skin/home/views.py
+++ skin/home/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "home/home.html")
+
+def list(request):
+    return render(request, "home/list.html")
+
+def make(request):
+    return render(request, "home/make.html")
+
+def use(request):
+    return render(request, "home/use.html")
